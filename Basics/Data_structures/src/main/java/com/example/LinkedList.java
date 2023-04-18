3a2c0d4e0ef88be7319378a3d9055c077976b00b
rikerik <bako.erik.dominik@gmail.com>
2023-04-18 18:55:46 +0200

    Linked list add

--- Basics/Data_structures/src/main/java/com/example/LinkedList.java
+++ Basics/Data_structures/src/main/java/com/example/LinkedList.java
@@ -1,10 +1,11 @@
-package main.java.com.example;
+package com.example;
+
 
 //LINKED LISTS
 //Search O(n)
 //implementation of Linked list
 
-class LinkedList {
+class LinkedLists {
     Node head; // the head of the list
 
     // Linked list Node. The inner class has to be static so main can acces it
@@ -31,15 +32,90 @@ class LinkedList {
     }
 }
 
+// Class for doubly linked list
+class DLL {
+    Node head;
+
+    // Doubly linked list node
+    static class Node {
+        int data;
+        Node prev;
+        Node next;
+
+        // next and prev is NULL
+        Node(int d) {
+            data = d;
+        }
+    }
+
+    // push function
+    public void push(int new_data) {
+
+        // allocate node and put in the data
+        Node new_node = new Node(new_data);
+
+        // make next node of the new node as head and previous as NULL
+        new_node.next = head;
+        new_node.prev = null;
+
+        // change prev of head node to new node linking back
+        if (head != null) {
+            head.prev = new_node;
+        }
+
+        // the new node become the new head node
+        head = new_node;
+    }
+
+    // add a note at the end of the list
+    void append(int new_data) {
+
+        // allocating node and put in data
+        Node new_node = new Node(new_data);
+
+        Node last = head; // used when traversing so we start with the first
+
+        // this new node is going to be the last node so it has NULL as next
+        new_node.next = null;
+
+        // if the linked list is empty make the new node as head
+        if (head == null) {
+            new_node.prev = null;
+            head = new_node;
+            return;
+        }
+
+        // else traverse till the last node
+        while (last.next != null) {
+            last = last.next;
+        }
+
+        // change the next of the last node
+        last.next = new_node;
+
+        // make the last node as prev of the new node
+        new_node.prev = last;
+
+    }
+
+    public void printList() {
+        Node n = head;
+        while (n != null) {
+            System.out.println(n.data + " ");
+            n = n.next;
+        }
+    }
+}
+
 public class LinkedList {
     public static void main(String[] args) {
 
         // Starting with an empty list
-        LinkedList lList = new LinkedList();
+        LinkedLists lList = new LinkedLists();
 
-        lList.head = new Node(1);
-        Node second = new Node(2);
-        Node third = new Node(3);
+        lList.head = new LinkedLists.Node(1);
+        LinkedLists.Node second = new LinkedLists.Node(2);
+        LinkedLists.Node third = new LinkedLists.Node(3);
 
         // link the first node to the second
         lList.head.next = second;
@@ -48,7 +124,15 @@ public class LinkedList {
         second.next = third;
 
         // function call
-        lList.printList();
+        // lList.printList();
 
+        DLL dllList = new DLL();
+
+        dllList.head = new DLL.Node(1);
+        dllList.push(2);
+        dllList.push(3);
+        dllList.append(12);
+
+        dllList.printList();
     }
 }
