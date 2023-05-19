44fa5aaa9cb91dc3f8dd6a5ca163de9e2be13efc
rikerik <bako.erik.dominik@gmail.com>
2023-05-19 20:47:14 +0200

    is alive method

---
+++ Advanced/Threads/isAliveClass.java
@@ -0,0 +1,23 @@
+package Threads;
+
+public class isAliveClass extends Thread {
+    public static int amount = 0;
+
+    public static void main(String[] args) {
+        isAliveClass thread = new isAliveClass();
+        thread.start();
+
+        //wait for the thread to finish
+        while (thread.isAlive()) {
+            System.out.println("Waiting...");
+        }
+        //Update amount and print its value
+        System.out.println("Main: " + amount);
+        amount++;
+        System.out.println("Main: " + amount);
+    }
+
+    public void run() {
+        amount++;
+    }
+}
