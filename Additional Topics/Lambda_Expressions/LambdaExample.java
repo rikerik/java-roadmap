a0f206a530858b37b1354c321f4c575368de47b2
rikerik <bako.erik.dominik@gmail.com>
2023-05-24 00:16:30 +0200

    Example for lambda expression

--- Additional Topics/Lambda_Expressions/LambdaExample.java
+++ Additional Topics/Lambda_Expressions/LambdaExample.java
@@ -1,2 +1,29 @@
-package Lambda_Expressions;public class LambdaExample {
+package Lambda_Expressions;
+
+//write a Java program that returns the value of Pi using the lambda expression.
+public class LambdaExample {
+
+    //this is a functional interface
+    @FunctionalInterface
+    interface MyInterface {
+        //abstract method
+        double getPiValue();
+    }
+    //We have created a functional interface named MyInterface.
+    //It contains a single abstract method named getPiValue()
+
+    public static void main(String[] args) {
+        //declare reference to MyInterface
+        MyInterface ref;
+
+        //lambda expression
+        ref = () -> 3.1415;
+        //We then assigned a lambda expression to the reference.
+
+        System.out.println("Value of Pi = " + ref.getPiValue());
+
+        //We have declared a reference to MyInterface.
+        //Note that we can declare a reference of an interface, but we cannot
+        //instantiate an interface.
+    }
 }
