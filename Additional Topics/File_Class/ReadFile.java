97c3245d4f311f326a0510ae867c1b3622f2c244
rikerik <bako.erik.dominik@gmail.com>
2023-05-28 13:46:11 +0200

    Read file

---
+++ Additional Topics/File_Class/ReadFile.java
@@ -0,0 +1,23 @@
+package File_Class;
+
+import java.io.FileReader;
+
+public class ReadFile {
+    public static void main(String[] args) {
+        System.out.println("Current working directory: " + System.getProperty("user.dir"));
+
+        char[] array = new char[100];
+        try {
+            //create a reader using the FileReader
+            FileReader input = new FileReader("File_Class/input.txt");
+            //reads characters
+            input.read(array);
+            System.out.println("Data in the file");
+            System.out.println(array);
+
+            input.close();
+        } catch (Exception e) {
+       throw new RuntimeException(e);
+        }
+    }
+}
