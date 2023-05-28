fc3f3594174629f2d5a6ea3900a2c0c92fc38698
rikerik <bako.erik.dominik@gmail.com>
2023-05-28 12:51:34 +0200

    File creation

--- Additional Topics/File_Class/FileClass.java
+++ Additional Topics/File_Class/FileClass.java
@@ -1,7 +1,22 @@
 package File_Class;
 
+import java.io.File;
+
 public class FileClass {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        //Creating a new file
+        //The file object is linked with the specified file path.
+        File file = new File(("newFile.txt"));
+
+        try {
+
+            //trying to create a file based on the object
+            boolean value = file.createNewFile();
+            if (value) System.out.println("The new file is created");
+            else System.out.println("The file already exists");
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
     }
 }
