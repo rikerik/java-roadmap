dd36c3b58d54ac72c668c985123f49573c5ad6a3
rikerik <bako.erik.dominik@gmail.com>
2023-05-29 20:49:56 +0200

    Delete file

--- Additional Topics/File_Class/DeleteFile.java
+++ Additional Topics/File_Class/DeleteFile.java
@@ -1,2 +1,16 @@
-package File_Class;public class DeleteFile {
+package File_Class;
+
+import java.io.File;
+
+public class DeleteFile {
+    public static void main(String[] args){
+        //creates a file object
+        File file = new File("File.txt");
+
+        //deletes the file
+        //if the deletion was successful, then it will return true
+        boolean value = file.delete();
+        if(value) System.out.println("The file is deleted");
+        else System.out.println("The file is not deleted");
+    }
 }
