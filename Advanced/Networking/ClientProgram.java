fb9fcb3f479c50fdd2331a101f041f7ff0a98123
rikerik <bako.erik.dominik@gmail.com>
2023-05-30 20:14:06 +0200

    Additional comments

--- Advanced/Networking/ClientProgram.java
+++ Advanced/Networking/ClientProgram.java
@@ -5,6 +5,17 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+/*
+To connect to another machine we need a socket connection.
+A socket connection means the two machines have information about
+each other’s network location (IP Address) and TCP port.
+The java.net.Socket class represents a Socket.
+
+To communicate over a socket connection, streams are used to both input and output the data.
+
+The socket connection is closed explicitly once the message to the server is sent.
+In the program, the Client keeps reading input from a user and sends it to the server until “Over” is typed.
+ */
 
 class Client {
     //initialize socket and input output streams
@@ -13,10 +24,10 @@ class Client {
     private DataOutputStream out = null;
 
     //constructor to put ip address an port
-    public Client(String addres, int port) {
+    public Client(String address, int port) {
         //establish a connection
         try {
-            socket = new Socket(addres, port);
+            socket = new Socket(address, port);
             System.out.println("Connected");
 
             //takes input from terminal
