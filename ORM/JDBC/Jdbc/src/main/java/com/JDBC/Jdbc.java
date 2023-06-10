6789e97bc80a9ee574421d96c1526167434889dd
rikerik <bako.erik.dominik@gmail.com>
2023-06-10 23:07:36 +0200

    SQL added

--- ORM/JDBC/Jdbc/src/main/java/com/JDBC/Jdbc.java
+++ ORM/JDBC/Jdbc/src/main/java/com/JDBC/Jdbc.java
@@ -1,7 +1,19 @@
 package com.JDBC;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Jdbc {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        //Connecting to the database
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:h2:mem:;INIT=RUNSCRIPT FROM 'classpath:users.sql';");
+            System.out.println("Connection.isValid(0) = "+connection.isValid(0));
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
 
     }
 }
