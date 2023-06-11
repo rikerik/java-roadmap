e986bce9a001bdbedf2dc7a82b99199e0f4a5010
rikerik <bako.erik.dominik@gmail.com>
2023-06-11 14:52:30 +0200

    Added HikariCP

--- ORM/JDBC/Jdbc/src/main/java/com/JDBC/Jdbc.java
+++ ORM/JDBC/Jdbc/src/main/java/com/JDBC/Jdbc.java
@@ -1,13 +1,23 @@
 package com.JDBC;
 
-import java.sql.*;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Jdbc {
     public static void main(String[] args) {
 
+        // Create a DataSource object using HikariCP connection pool
+        DataSource dataSource = createDatasource();
+
         //Connecting to the database
         try {
-            Connection connection = DriverManager.getConnection("jdbc:h2:mem:;INIT=RUNSCRIPT FROM 'classpath:users.sql';");
+            // Obtain a connection from the DataSource
+            Connection connection = dataSource.getConnection();
             System.out.println("Connection.isValid(0) = " + connection.isValid(0));
 
             //CRUD
@@ -16,8 +26,9 @@ public class Jdbc {
 
             PreparedStatement preparedStatement = connection.prepareStatement("select * from USERS where name =?");
             preparedStatement.setString(1, "Erik");
-
             ResultSet resultSet = preparedStatement.executeQuery();
+
+            // Iterate through the result set and print user information
             while (resultSet.next()) {
                 System.out.println(resultSet.getInt("id") + " - " + resultSet.getString("name"));
             }
@@ -47,6 +58,17 @@ public class Jdbc {
         } catch (SQLException sqle) {
             sqle.printStackTrace();
         }
+
+    }
+
+    private static DataSource createDatasource() {
+        // Create a new instance of HikariDataSource
+        HikariDataSource ds = new HikariDataSource();
+        // Set the JDBC URL for the H2 in-memory database and initialize it with the "users.sql" script
+        ds.setJdbcUrl("jdbc:h2:mem:;INIT=RUNSCRIPT FROM 'classpath:users.sql';");
+        return ds;
+        //hikaricp -connection pool
+        //DriverManager.getConnection("jdbc:h2:mem:;INIT=RUNSCRIPT FROM 'classpath:users.sql';");
 
     }
 }
