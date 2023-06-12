6e08c6de03439a348bdb93ac7e24cc9631a54258
rikerik <bako.erik.dominik@gmail.com>
2023-06-12 22:57:40 +0200

    Config Datasource

--- ORM/jdbc-start/src/test/java/com/rikerik/DemoApplicationTests.java
+++ ORM/jdbc-start/src/test/java/com/rikerik/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.amigoscode;
+package com.rikerik;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
--- ORM/jdbc-start/src/test/java/com/rikerik/movie/MovieControllerTest.java
+++ ORM/jdbc-start/src/test/java/com/rikerik/movie/MovieControllerTest.java
@@ -1,4 +1,4 @@
-package com.amigoscode.movie;
+package com.rikerik.movie;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
