8e365e4890465c22a706db2d28aefba8e036f86c
rikerik <bako.erik.dominik@gmail.com>
2023-06-17 10:09:52 +0200

    Using postgres instead of h2

---
+++ ORM/Hibernate/hibernate/src/test/java/org/example/HibernateFullTest.java
@@ -0,0 +1,59 @@
+package org.example;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class HibernateFullTest {
+
+    private SessionFactory sessionFactory;
+
+    @BeforeEach
+    protected void setUp() throws Exception {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        } catch (Exception e) {
+            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+            // so destroy it manually.
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    @AfterEach
+    protected void tearDown() throws Exception {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testBasicUsage() {
+        User user = new User("Erik", LocalDate.now());
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.persist(user);
+        session.getTransaction().commit();
+
+
+    }
+}
+
+
