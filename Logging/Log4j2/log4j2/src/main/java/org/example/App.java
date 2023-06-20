42b47cebaba6ec683f5f41a7d8aab6ef27e951bd
rikerik <bako.erik.dominik@gmail.com>
2023-06-20 11:00:56 +0200

    Log4j2

---
+++ Logging/Log4j2/log4j2/src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App
+{
+
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World!");
+        logger.debug("Hello World!");
+    }
+}
